cb79888fb47a0eb1d23c797711d3f01ab16744da
Hyuk Myeong <hyuk.myeong@lge.com>
2021-04-10 15:30:57 +0900

    coming up

--- test/gtest_ex3.cpp
+++ test/gtest_ex3.cpp
@@ -1,3 +1,4 @@
+#if 1
 #include <iostream>
 #include <string>
 #include "gtest/gtest.h"
@@ -40,3 +41,43 @@ int main(int argc, char **argv)
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
+#else
+#include <iostream>
+#include <string>
+#include <thread>
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#if defined(MEMORY_LEAK_TEST) && defined(_MSC_VER)
+#include "vld.h"
+#endif
+
+using namespace std;
+using namespace ::testing;
+
+int foo(const string& thread_name)
+{
+  int* ptr = new int();
+}
+
+TEST(LeakTest, Thread)
+{
+  thread t1(foo, "t1");
+  thread t2(foo, "t2");
+
+  t1.join();
+  t2.join();
+}
+
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+#endif
